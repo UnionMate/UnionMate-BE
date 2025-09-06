8d2605333e24cd21cfa79cb282f66b37fc6237d0
senna <kjy915875@gmail.com>
2025-09-06 19:35:38 +0900

    feat: School 엔티티 구현

---
+++ src/main/java/com/unionmate/backend/domain/member/entity/School.java
@@ -0,0 +1,39 @@
+package com.unionmate.backend.domain.member.entity;
+
+import com.unionmate.backend.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(
+    name = "schools",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "uk_school_name",
+            columnNames = {"name"}
+        ),
+        @UniqueConstraint(
+            name = "uk_school_domain",
+            columnNames = {"domain"}
+        )
+    }
+)
+@Getter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class School extends BaseEntity {
+
+  @Column(name = "name", nullable = false, unique = true)
+  private String name;
+
+  @Column(name = "domain", nullable = false, unique = true)
+  private String domain;
+}
