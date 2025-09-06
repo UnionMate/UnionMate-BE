93d27e730ff50426fb18f6a3931fc342ad571f66
senna <kjy915875@gmail.com>
2025-09-06 19:37:32 +0900

    feat: 지원서 관련 엔티티, enum 및 Answer 들의 부모 엔티티 구현

---
+++ src/main/java/com/unionmate/backend/domain/applicant/entity/Application.java
@@ -0,0 +1,49 @@
+package com.unionmate.backend.domain.applicant.entity;
+
+import com.unionmate.backend.domain.applicant.entity.embed.Interview;
+import com.unionmate.backend.global.entity.BaseEntity;
+import com.unionmate.backend.domain.applicant.entity.answer.Answer;
+import com.unionmate.backend.domain.applicant.entity.embed.Stage;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "applications")
+@Getter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Application extends BaseEntity {
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "email", nullable = false)
+  private String email;
+
+  @Column(name = "tel", nullable = false)
+  private String tel;
+
+  @Embedded
+  @Builder.Default
+  private Interview interview = Interview.init();
+
+  @Embedded
+  @Builder.Default
+  private Stage stage = Stage.init();
+
+  @OneToMany(mappedBy = "application")
+  @Builder.Default
+  private List<Answer> answers = new ArrayList<>();
+}
---
+++ src/main/java/com/unionmate/backend/domain/applicant/entity/embed/Stage.java
@@ -0,0 +1,41 @@
+package com.unionmate.backend.domain.applicant.entity.embed;
+
+import com.unionmate.backend.domain.applicant.entity.enums.EvaluationStatus;
+import com.unionmate.backend.domain.recruitment.entity.enums.RecruitmentStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+@Embeddable
+public record Stage(
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    RecruitmentStatus recruitmentStatus,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    EvaluationStatus evaluationStatus
+) {
+
+  public Stage failOnThisStage() {
+    return new Stage(this.recruitmentStatus, EvaluationStatus.FAILED);
+  }
+
+  public Stage passOnThisStage() {
+    return new Stage(this.recruitmentStatus, EvaluationStatus.PASSED);
+  }
+
+  public Stage toNextStage() {
+    switch (this.recruitmentStatus) {
+      case DOCUMENT_SCREENING ->  new Stage(RecruitmentStatus.INTERVIEW, EvaluationStatus.SUBMITTED);
+      case INTERVIEW -> new Stage(RecruitmentStatus.FINAL, EvaluationStatus.SUBMITTED);
+    }
+    // TODO: 예외 처리 로직 확정되면 예외 처리
+    throw new IllegalStateException();
+  }
+
+  public static Stage init() {
+    return new Stage(RecruitmentStatus.DOCUMENT_SCREENING, EvaluationStatus.SUBMITTED);
+  }
+}
