2ffa0605b3c4750490fa433e08d39be488ab76f5
senna <kjy915875@gmail.com>
2025-09-08 14:38:38 +0900

    feat: 예외 헨들러 추가

---
+++ src/main/java/com/unionmate/exception/handler/ExceptionUtil.java
@@ -0,0 +1,16 @@
+package com.unionmate.exception.handler;
+
+public abstract class ExceptionUtil {
+
+  public static boolean isClientAbort(Throwable e) {
+    int depth = 0;
+    while (e != null && depth < 8) {
+      if (e instanceof org.apache.catalina.connector.ClientAbortException) {
+        return true;
+      }
+      e = e.getCause();
+      depth++;
+    }
+    return false;
+  }
+}
