c1de43d7c7b44c66b19091ebaaeb522365da7235
1winhyun <jimking1@naver.com>
2025-10-09 16:14:39 +0900

    refactor: CreateItemRequest 필드 검증 추가

--- src/main/java/com/unionmate/backend/domain/recruitment/application/dto/request/CreateItemRequest.java
+++ src/main/java/com/unionmate/backend/domain/recruitment/application/dto/request/CreateItemRequest.java
@@ -5,11 +5,19 @@ import java.util.List;
 
 import com.unionmate.backend.domain.recruitment.domain.entity.enums.ItemType;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record CreateItemRequest(
+	@NotNull(message = "항목의 타입 지정은 필수입니다.")
 	ItemType type,
+	@NotNull(message = "필수 작성 항목 여부 지정은 필수입니다.")
 	Boolean required,
+	@NotBlank(message = "항목의 이름은 필수입니다.")
 	String title,
+	@NotNull(message = "항목의 순서 지정은 필수입니다.")
 	Integer order,
+	@NotBlank(message = "해당 항목에 대한 설명은 필수입니다.")
 	String description,
 
 	//Select용
