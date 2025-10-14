c8be672140c4cbd90cd61edded7dc64a396eea3d
1winhyun <jimking1@naver.com>
2025-10-14 17:28:26 +0900

    feat: Calendar 형식 답변 request dto 작성

--- src/main/java/com/unionmate/backend/domain/applicant/dto/request/AnswerRequest.java
+++ src/main/java/com/unionmate/backend/domain/applicant/dto/request/AnswerRequest.java
@@ -2,7 +2,7 @@ package com.unionmate.backend.domain.applicant.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 
-public sealed interface AnswerRequest permits TextAnswerRequest, SelectAnswerRequest, CalendatAnswerRequest {
+public sealed interface AnswerRequest permits TextAnswerRequest, SelectAnswerRequest, CalendarAnswerRequest {
 	@NotNull
 	Long itemId();
 }
