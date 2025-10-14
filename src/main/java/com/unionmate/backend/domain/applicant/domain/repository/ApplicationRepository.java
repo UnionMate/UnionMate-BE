8c5e95a53531a02fc8e292f8f43f4cdb7d95d1e3
1winhyun <jimking1@naver.com>
2025-10-14 20:14:39 +0900

    지원서 저장 service 작성

--- src/main/java/com/unionmate/backend/domain/applicant/domain/repository/ApplicationRepository.java
+++ src/main/java/com/unionmate/backend/domain/applicant/domain/repository/ApplicationRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.unionmate.backend.domain.applicant.domain.entity.Application;
 
-public interface ApplicantRepository extends JpaRepository<Application, Long> {
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
 }
