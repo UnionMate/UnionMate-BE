aec09433b2e82d05ab53b0badb99272fa9bddaac
1winhyun <jimking1@naver.com>
2025-10-15 04:11:53 +0900

    refactor: 역직렬화 타입 확인 불가 오류 해결

--- src/main/java/com/unionmate/backend/domain/applicant/application/dto/request/AnswerRequest.java
+++ src/main/java/com/unionmate/backend/domain/applicant/application/dto/request/AnswerRequest.java
@@ -1,7 +1,16 @@
 package com.unionmate.backend.domain.applicant.application.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import jakarta.validation.constraints.NotNull;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+@JsonSubTypes({
+	@JsonSubTypes.Type(value = TextAnswerRequest.class),
+	@JsonSubTypes.Type(value = SelectAnswerRequest.class),
+	@JsonSubTypes.Type(value = CalendarAnswerRequest.class)
+})
 public sealed interface AnswerRequest permits TextAnswerRequest, SelectAnswerRequest, CalendarAnswerRequest {
 	@NotNull
 	Long itemId();
