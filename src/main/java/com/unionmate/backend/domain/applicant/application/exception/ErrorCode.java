553c3ee9c08e5672bae4142059619b77fb4b5026
1winhyun <jimking1@naver.com>
2025-10-16 00:58:03 +0900

    refactor: 오류 코드 값 불일치 수정

--- src/main/java/com/unionmate/backend/domain/applicant/application/exception/ErrorCode.java
+++ src/main/java/com/unionmate/backend/domain/applicant/application/exception/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode implements ErrorInfo {
 	ITEM_NOT_FOUND("해당 문항을 찾을 수 없습니다.", 2102),
 	ITEM_TYPE_MISMATCH("문항 형식이 일치하지 않습니다.", 2103),
 	REQUIRED_ANSWER_MISSING("필수 문항의 답변이 누락되었습니다.", 2104),
-	TEXT_TOO_LONG("최대 글자 수를 초과했습니다.", 2015),
+	TEXT_TOO_LONG("최대 글자 수를 초과했습니다.", 2105),
 	OPTION_INVALID("유효하지 않은 선택지입니다.", 2106),
 	RECRUITMENT_CLOSED("현재 지원 기간이 아닙니다.", 2107);
 
