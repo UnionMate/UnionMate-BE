bc77bb27f575d58e8e8d1b31eb1ea062f9a168b3
rootTiket <l20078829@gmail.com>
2025-10-21 21:03:19 +0900

    refactor: 불필요한 transactional 삭제

--- src/main/java/com/unionmate/backend/domain/council/domain/service/CouncilSaveService.java
+++ src/main/java/com/unionmate/backend/domain/council/domain/service/CouncilSaveService.java
@@ -1,14 +1,12 @@
 package com.unionmate.backend.domain.council.domain.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.unionmate.backend.domain.council.domain.entity.Council;
 import com.unionmate.backend.domain.council.domain.repository.CouncilRepository;
 
 import lombok.RequiredArgsConstructor;
 
-@Transactional
 @RequiredArgsConstructor
 @Service
 public class CouncilSaveService {
