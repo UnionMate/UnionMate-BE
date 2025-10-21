b39afa3765a7e76759e6a4206dae5be26311100d
rootTiket <l20078829@gmail.com>
2025-10-21 21:27:07 +0900

    feat: CouncilManager 조회 기능 추가

--- src/main/java/com/unionmate/backend/domain/council/domain/repository/CouncilManagerRepository.java
+++ src/main/java/com/unionmate/backend/domain/council/domain/repository/CouncilManagerRepository.java
@@ -1,5 +1,7 @@
 package com.unionmate.backend.domain.council.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,5 @@ import com.unionmate.backend.domain.member.domain.entity.Member;
 @Repository
 public interface CouncilManagerRepository extends JpaRepository<CouncilManager, Long> {
 	boolean existsByMember(Member member);
+	Optional<CouncilManager> findByMember(Member member);
 }
--- src/main/java/com/unionmate/backend/domain/council/domain/service/CouncilManagerGetService.java
+++ src/main/java/com/unionmate/backend/domain/council/domain/service/CouncilManagerGetService.java
@@ -2,7 +2,9 @@ package com.unionmate.backend.domain.council.domain.service;
 
 import org.springframework.stereotype.Service;
 
+import com.unionmate.backend.domain.council.domain.entity.CouncilManager;
 import com.unionmate.backend.domain.council.domain.repository.CouncilManagerRepository;
+import com.unionmate.backend.domain.council.exception.CouncilManagerNotFoundException;
 import com.unionmate.backend.domain.member.domain.entity.Member;
 
 import lombok.RequiredArgsConstructor;
@@ -14,5 +16,15 @@ public class CouncilManagerGetService {
 
 	public boolean existsByMember(Member member) {
 		return councilManagerRepository.existsByMember(member);
+	}
+
+	public CouncilManager getCouncilManagerByMember(Member member) {
+		return councilManagerRepository.findByMember(member)
+			.orElseThrow(CouncilManagerNotFoundException::new);
+	}
+
+	public CouncilManager getCouncilManager(long councilManagerId) {
+		return councilManagerRepository.findById(councilManagerId)
+			.orElseThrow(CouncilManagerNotFoundException::new);
 	}
 }
