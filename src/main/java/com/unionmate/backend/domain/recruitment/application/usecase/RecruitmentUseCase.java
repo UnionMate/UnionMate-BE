318074d11e6b6f9db59b3071f0a3496f083d8470
1winhyun <jimking1@naver.com>
2025-10-23 00:08:37 +0900

    refactor: 엔티티 내 구현된 builder 적용

--- src/main/java/com/unionmate/backend/domain/recruitment/application/usecase/RecruitmentUseCase.java
+++ src/main/java/com/unionmate/backend/domain/recruitment/application/usecase/RecruitmentUseCase.java
@@ -7,11 +7,16 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.unionmate.backend.domain.recruitment.application.dto.request.CreateItemRequest;
 import com.unionmate.backend.domain.recruitment.application.dto.request.CreateRecruitmentRequest;
+import com.unionmate.backend.domain.recruitment.application.dto.request.SelectOptionRequest;
 import com.unionmate.backend.domain.recruitment.application.dto.response.RecruitmentResponse;
 import com.unionmate.backend.domain.recruitment.application.mapper.RecruitmentResponseMapper;
-import com.unionmate.backend.domain.recruitment.application.mapper.RecruitmentRequestMapper;
 import com.unionmate.backend.domain.recruitment.domain.entity.Recruitment;
+import com.unionmate.backend.domain.recruitment.domain.entity.item.AnnouncementItem;
+import com.unionmate.backend.domain.recruitment.domain.entity.item.CalendarItem;
 import com.unionmate.backend.domain.recruitment.domain.entity.item.Item;
+import com.unionmate.backend.domain.recruitment.domain.entity.item.SelectItem;
+import com.unionmate.backend.domain.recruitment.domain.entity.item.SelectItemOption;
+import com.unionmate.backend.domain.recruitment.domain.entity.item.TextItem;
 import com.unionmate.backend.domain.recruitment.domain.service.RecruitmentGetService;
 import com.unionmate.backend.domain.recruitment.domain.service.RecruitmentSaveService;
 
@@ -20,19 +25,18 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class RecruitmentUseCase {
-	private final RecruitmentRequestMapper recruitmentRequestMapper;
 	private final RecruitmentSaveService recruitmentSaveService;
 	private final RecruitmentGetService recruitmentGetService;
 	private final RecruitmentResponseMapper recruitmentResponseMapper;
 
 	@Transactional
 	public void createRecruitment(CreateRecruitmentRequest rq) {
-		Recruitment recruitment = recruitmentRequestMapper
-			.toRecruitment(rq.name(), LocalDateTime.now(), rq.endAt(), rq.isActive(), rq.recruitmentStatus());
+		Recruitment recruitment = Recruitment.createRecruitment(rq.name(), LocalDateTime.now(), rq.endAt(),
+			rq.isActive(), rq.recruitmentStatus());
 
 		if (rq.items() != null) {
 			for (CreateItemRequest ir : rq.items()) {
-				Item item = recruitmentRequestMapper.toItem(recruitment, ir);
+				Item item = createItem(recruitment, ir);
 				recruitment.getItems().add(item);
 			}
 		}
@@ -43,5 +47,37 @@ public class RecruitmentUseCase {
 		Recruitment recruitment = recruitmentGetService.getRecruitmentById(id);
 
 		return recruitmentResponseMapper.toRecruitmentResponse(recruitment);
+	}
+
+	private Item createItem(Recruitment recruitment, CreateItemRequest createItemRequest) {
+		boolean required = Boolean.TRUE.equals(createItemRequest.required());
+		boolean multiple = Boolean.TRUE.equals(createItemRequest.multiple());
+
+		return switch (createItemRequest.type()) {
+			case TEXT -> TextItem.createRecruitmentText(recruitment, required, createItemRequest.title(),
+				createItemRequest.order(), createItemRequest.description(), createItemRequest.maxLength());
+
+			case SELECT -> {
+				SelectItem selectItem = SelectItem.createRecruitmentSelect(recruitment, required,
+					createItemRequest.title(), createItemRequest.order(), createItemRequest.description(), multiple);
+
+				if (createItemRequest.options() != null) {
+					for (SelectOptionRequest selectOptionRequest : createItemRequest.options()) {
+						selectItem.getSelectItemOptions().add(
+							SelectItemOption.createRecruitmentSelectOption(selectOptionRequest.title(),
+								selectOptionRequest.order(), Boolean.TRUE.equals(selectOptionRequest.isEtc()),
+								selectOptionRequest.etcTitle(), selectItem));
+					}
+				}
+				yield selectItem;
+			}
+
+			case CALENDAR -> CalendarItem.createRecruitmentCalendar(recruitment, required, createItemRequest.title(),
+				createItemRequest.order(), createItemRequest.description(), createItemRequest.date());
+
+			case ANNOUNCEMENT ->
+				AnnouncementItem.createRecruitmentAnnouncement(recruitment, required, createItemRequest.title(),
+					createItemRequest.order(), createItemRequest.description(), createItemRequest.announcement());
+		};
 	}
 }
