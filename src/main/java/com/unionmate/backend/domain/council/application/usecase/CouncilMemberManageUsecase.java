4e8515e9fabfe2af3eaf6a269b1893aedb0ad902
rootTiket <l20078829@gmail.com>
2025-10-22 19:53:27 +0900

    feat: 학생회 구성원 조회 기능 추가 - usecase 구현

--- src/main/java/com/unionmate/backend/domain/council/application/usecase/CouncilMemberManageUsecase.java
+++ src/main/java/com/unionmate/backend/domain/council/application/usecase/CouncilMemberManageUsecase.java
@@ -1,9 +1,14 @@
 package com.unionmate.backend.domain.council.application.usecase;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.unionmate.backend.domain.council.application.dto.CouncilMemberResponse;
+import com.unionmate.backend.domain.council.domain.entity.Council;
 import com.unionmate.backend.domain.council.domain.entity.CouncilManager;
+import com.unionmate.backend.domain.council.domain.service.CouncilGetService;
 import com.unionmate.backend.domain.council.domain.service.CouncilManagerGetService;
 import com.unionmate.backend.domain.member.domain.entity.Member;
 import com.unionmate.backend.domain.member.domain.service.MemberGetService;
@@ -16,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class CouncilMemberManageUsecase {
 	private final MemberGetService memberGetService;
 	private final CouncilManagerGetService councilManagerGetService;
+	private final CouncilGetService councilGetService;
 
 	@Transactional
 	public void delegateVice(long userId, long newPresidentId) {
@@ -29,5 +35,14 @@ public class CouncilMemberManageUsecase {
 
 		currentManager.delegateToMember();
 		newManager.promoteToVice();
+	}
+
+	public List<CouncilMemberResponse> getAllCouncilMembers(long councilId) {
+		Council council = councilGetService.getCouncilById(councilId);
+
+		return councilManagerGetService.getAllCouncilMembers(council)
+			.stream()
+			.map(CouncilMemberResponse::from)
+			.toList();
 	}
 }
