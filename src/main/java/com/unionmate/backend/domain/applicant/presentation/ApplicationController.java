942f6865d0b32c63985cd8a08908509f415590a6
1winhyun <jimking1@naver.com>
2025-10-24 20:25:50 +0900

    feat: 지원서 목록 조회 관련 Controller 코드 구현

--- src/main/java/com/unionmate/backend/domain/applicant/presentation/ApplicationController.java
+++ src/main/java/com/unionmate/backend/domain/applicant/presentation/ApplicationController.java
@@ -1,5 +1,8 @@
 package com.unionmate.backend.domain.applicant.presentation;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.unionmate.backend.domain.applicant.application.dto.request.CreateApplicantRequest;
+import com.unionmate.backend.domain.applicant.application.dto.response.GetMyApplicationsResponse;
 import com.unionmate.backend.domain.applicant.application.usecase.ApplicationUseCase;
 import com.unionmate.backend.global.response.CommonResponse;
 
@@ -27,5 +31,14 @@ public class ApplicationController {
 		applicationUseCase.submitApplication(recruitmentId, createApplicantRequest);
 
 		return CommonResponse.success(ApplicationResponseCode.SUBMIT_APPLICATION);
+	}
+
+	@GetMapping("/{name}/{email}")
+	@Operation(summary = "자신이 작성한 지원서 목록을 조회합니다.")
+	public CommonResponse<List<GetMyApplicationsResponse>> getMyApplications(
+		@PathVariable String name, @PathVariable String email) {
+		List<GetMyApplicationsResponse> myApplications = applicationUseCase.getMyApplications(name, email);
+
+		return CommonResponse.success(ApplicationResponseCode.GET_MY_APPLICATIONS, myApplications);
 	}
 }
