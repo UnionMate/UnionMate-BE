873d9d0f62b1737ff3bff03d36d2c98193d56088
1winhyun <jimking1@naver.com>
2025-10-24 23:10:45 +0900

    feat: 지원서 조회 response dto 정적 팩토리 메서드 구현

--- src/main/java/com/unionmate/backend/domain/applicant/application/dto/response/GetApplicationResponse.java
+++ src/main/java/com/unionmate/backend/domain/applicant/application/dto/response/GetApplicationResponse.java
@@ -1,6 +1,10 @@
 package com.unionmate.backend.domain.applicant.application.dto.response;
 
+import java.util.Comparator;
 import java.util.List;
+
+import com.unionmate.backend.domain.applicant.domain.entity.Application;
+import com.unionmate.backend.domain.recruitment.domain.entity.item.Item;
 
 public record GetApplicationResponse(
 	Long applicationId,
@@ -8,5 +12,17 @@ public record GetApplicationResponse(
 	String recruitmentName,
 	List<ApplicationAnswerResponse> answers
 ) {
+	public static GetApplicationResponse from(Application application) {
+		List<ApplicationAnswerResponse> answer = application.getAnswers().stream()
+			.sorted(Comparator.comparing(Item::getOrder))
+			.map(ApplicationAnswerResponse::from)
+			.toList();
 
+		return new GetApplicationResponse(
+			application.getId(),
+			application.getRecruitment().getId(),
+			application.getRecruitment().getName(),
+			answer
+		);
+	}
 }
