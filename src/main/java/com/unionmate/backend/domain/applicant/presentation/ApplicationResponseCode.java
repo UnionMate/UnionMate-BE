5b86a44bd148e3f99c5e76e3d0f76dadf91693a9
1winhyun <jimking1@naver.com>
2025-10-24 18:19:54 +0900

    feat: 작성한 지원서 조회 관련 응답코드 구현

--- src/main/java/com/unionmate/backend/domain/applicant/presentation/ApplicationResponseCode.java
+++ src/main/java/com/unionmate/backend/domain/applicant/presentation/ApplicationResponseCode.java
@@ -10,7 +10,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ApplicationResponseCode implements ResponseCodeInterface {
-	SUBMIT_APPLICATION(200, HttpStatus.OK, "지원서가 성공적으로 제출되었습니다.");
+	SUBMIT_APPLICATION(200, HttpStatus.OK, "지원서가 성공적으로 제출되었습니다."),
+	GET_MY_APPLICATIONS(200, HttpStatus.OK, "본인이 작성한 지원서 목록 조회에 성공했습니다."),
+	GET_MY_APPLICATION(200, HttpStatus.OK, "본인이 작성한 지원서 조회에 성공했습니다.");
 
 	private final int code;
 	private final HttpStatus status;
