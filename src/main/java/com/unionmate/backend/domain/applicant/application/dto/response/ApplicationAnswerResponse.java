336ce8614710748970b2e304362e94987adf12de
1winhyun <jimking1@naver.com>
2025-10-24 21:59:38 +0900

    feat: 지원서 응답 조회 response dto 정적 팩토리 메서드 구현

--- src/main/java/com/unionmate/backend/domain/applicant/application/dto/response/ApplicationAnswerResponse.java
+++ src/main/java/com/unionmate/backend/domain/applicant/application/dto/response/ApplicationAnswerResponse.java
@@ -1,6 +1,11 @@
 package com.unionmate.backend.domain.applicant.application.dto.response;
 
+import com.unionmate.backend.domain.recruitment.application.exception.ItemTypeNotExistException;
 import com.unionmate.backend.domain.recruitment.domain.entity.enums.ItemType;
+import com.unionmate.backend.domain.recruitment.domain.entity.item.CalendarItem;
+import com.unionmate.backend.domain.recruitment.domain.entity.item.Item;
+import com.unionmate.backend.domain.recruitment.domain.entity.item.SelectItem;
+import com.unionmate.backend.domain.recruitment.domain.entity.item.TextItem;
 
 public sealed interface ApplicationAnswerResponse
 	permits TextAnswerResponse, SelectAnswerResponse, CalendarAnswerResponse {
@@ -11,4 +16,13 @@ public sealed interface ApplicationAnswerResponse
 	Integer order();
 
 	String description();
+
+	static ApplicationAnswerResponse from(Item item) {
+		return switch (item) {
+			case TextItem textItem -> TextAnswerResponse.from(textItem);
+			case SelectItem selectItem -> SelectAnswerResponse.from(selectItem);
+			case CalendarItem calendarItem -> CalendarAnswerResponse.from(calendarItem);
+			default -> throw new ItemTypeNotExistException();
+		};
+	}
 }
