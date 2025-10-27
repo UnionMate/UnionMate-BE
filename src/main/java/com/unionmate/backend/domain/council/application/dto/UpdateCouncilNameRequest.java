f8625d6258fbcd10d4a64b8ceb4ed5bcd6be1e53
rootTiket <l20078829@gmail.com>
2025-10-27 21:13:03 +0900

    refactor: String 검사 강화

--- src/main/java/com/unionmate/backend/domain/council/application/dto/UpdateCouncilNameRequest.java
+++ src/main/java/com/unionmate/backend/domain/council/application/dto/UpdateCouncilNameRequest.java
@@ -1,9 +1,9 @@
 package com.unionmate.backend.domain.council.application.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record UpdateCouncilNameRequest(
-	@NotNull
+	@NotBlank
 	String name
 ) {
 }
