7c792533a7cfeae021ac90d21fa9bbf52e1d192b
1winhyun <jimking1@naver.com>
2025-10-28 12:17:28 +0900

    refactor: UpdateApplicationRequest 설명 추가

--- src/main/java/com/unionmate/backend/domain/applicant/application/dto/request/UpdateApplicationRequest.java
+++ src/main/java/com/unionmate/backend/domain/applicant/application/dto/request/UpdateApplicationRequest.java
@@ -2,10 +2,19 @@ package com.unionmate.backend.domain.applicant.application.dto.request;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UpdateApplicationRequest(
+	@Schema(description = "지원자 이름", example = "김가천")
 	String name,
+
+	@Schema(description = "지원자 이메일", example = "unionmate@unionmate.com")
 	String email,
+
+	@Schema(description = "지원자 연락처", example = "010123411234")
 	String tel,
+
+	@Schema(description = "문항별 답변 목록")
 	List<AnswerRequest> answers
 ) {
 }
