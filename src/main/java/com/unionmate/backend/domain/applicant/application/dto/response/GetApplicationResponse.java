21c555860e184afe2f20ca1876723453916930ff
1winhyun <jimking1@naver.com>
2025-10-28 20:33:53 +0900

    refactor: 지원서 관련 response dto 스키마 어노테이션 추가

--- src/main/java/com/unionmate/backend/domain/applicant/application/dto/response/GetApplicationResponse.java
+++ src/main/java/com/unionmate/backend/domain/applicant/application/dto/response/GetApplicationResponse.java
@@ -6,12 +6,25 @@ import java.util.List;
 import com.unionmate.backend.domain.applicant.domain.entity.Application;
 import com.unionmate.backend.domain.recruitment.domain.entity.item.Item;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record GetApplicationResponse(
+	@Schema(description = "지원서 id", example = "1")
 	Long applicationId,
+
+	@Schema(description = "지원서 양식 id", example = "1")
 	Long recruitmentId,
+
+	@Schema(description = "지원서 이름", example = "25대 컴퓨터공학과 학생회 모집")
 	String recruitmentName,
+
+	@Schema(description = "지원자 이름", example = "김가천")
 	String name,
+
+	@Schema(description = "지원자 이메일", example = "unionmate@unionmate.com")
 	String email,
+
+	@Schema(description = "지원자 전화번호", example = "010-6658-6316")
 	String tel,
 	List<ApplicationAnswerResponse> answers
 ) {
--- src/main/java/com/unionmate/backend/domain/applicant/application/dto/response/SelectAnswerResponse.java
+++ src/main/java/com/unionmate/backend/domain/applicant/application/dto/response/SelectAnswerResponse.java
@@ -5,12 +5,25 @@ import java.util.List;
 import com.unionmate.backend.domain.recruitment.domain.entity.enums.ItemType;
 import com.unionmate.backend.domain.recruitment.domain.entity.item.SelectItem;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record SelectAnswerResponse(
+	@Schema(description = "항목 타입 (TEXT, SELECT, CALENDAR, ANNOUNCEMENT)", example = "SELECT")
 	ItemType itemType,
+
+	@Schema(description = "항목 제목", example = "날짜 선택")
 	String title,
+
+	@Schema(description = "표시 순서 (1부터 시작)", example = "1")
 	Integer order,
+
+	@Schema(description = "항목 설명 안내", example = "해당 날짜 전까지 희망하는 날짜를 작성해주세요")
 	String description,
+
+	@Schema(description = "중복 선택 가능 여부 ", example = "false")
 	boolean multiple,
+
+	@Schema(description = "답변한 선택지")
 	List<Long> selectedOptionIds
 ) implements ApplicationAnswerResponse {
 
