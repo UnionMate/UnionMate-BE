53f9198ddfc0df2c2dab18e8ea09a76f41e4bf1d
huncozyboy <leejh5838@naver.com>
2025-10-30 00:26:11 +0900

    refactor #28 : 서류,면접 평가 상태 변경 메서드 수정

--- src/main/java/com/unionmate/backend/domain/applicant/domain/entity/embed/Stage.java
+++ src/main/java/com/unionmate/backend/domain/applicant/domain/entity/embed/Stage.java
@@ -2,6 +2,7 @@ package com.unionmate.backend.domain.applicant.domain.entity.embed;
 
 import com.unionmate.backend.domain.applicant.domain.entity.enums.EvaluationStatus;
 import com.unionmate.backend.domain.recruitment.domain.entity.enums.RecruitmentStatus;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
@@ -9,37 +10,45 @@ import jakarta.persistence.Enumerated;
 
 @Embeddable
 public record Stage(
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    RecruitmentStatus recruitmentStatus,
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	RecruitmentStatus recruitmentStatus,
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    EvaluationStatus evaluationStatus
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	EvaluationStatus evaluationStatus
 ) {
 
-  public Stage failOnThisStage() {
-    return new Stage(this.recruitmentStatus, EvaluationStatus.FAILED);
-  }
+	public static Stage finalizeFailed() {
+		return new Stage(RecruitmentStatus.FINAL, EvaluationStatus.FAILED);
+	}
 
-  public Stage passOnThisStage() {
-    return new Stage(this.recruitmentStatus, EvaluationStatus.PASSED);
-  }
+	public static Stage finalizePassed() {
+		return new Stage(RecruitmentStatus.FINAL, EvaluationStatus.PASSED);
+	}
 
-  public Stage toNextStage() {
-    switch (this.recruitmentStatus) {
-      case DOCUMENT_SCREENING -> {
-        return new Stage(RecruitmentStatus.INTERVIEW, EvaluationStatus.SUBMITTED);
-      }
-      case INTERVIEW -> {
-        return new Stage(RecruitmentStatus.FINAL, EvaluationStatus.SUBMITTED);
-      }
-    }
-    // TODO: 예외 처리 로직 확정되면 예외 처리
-    throw new IllegalStateException();
-  }
+	public static Stage init() {
+		return new Stage(RecruitmentStatus.DOCUMENT_SCREENING, EvaluationStatus.SUBMITTED);
+	}
 
-  public static Stage init() {
-    return new Stage(RecruitmentStatus.DOCUMENT_SCREENING, EvaluationStatus.SUBMITTED);
-  }
+	public Stage failOnThisStage() {
+		return new Stage(this.recruitmentStatus, EvaluationStatus.FAILED);
+	}
+
+	public Stage passOnThisStage() {
+		return new Stage(this.recruitmentStatus, EvaluationStatus.PASSED);
+	}
+
+	public Stage toNextStage() {
+		switch (this.recruitmentStatus) {
+			case DOCUMENT_SCREENING -> {
+				return new Stage(RecruitmentStatus.INTERVIEW, EvaluationStatus.SUBMITTED);
+			}
+			case INTERVIEW -> {
+				return new Stage(RecruitmentStatus.FINAL, EvaluationStatus.SUBMITTED);
+			}
+		}
+		// TODO: 예외 처리 로직 확정되면 예외 처리
+		throw new IllegalStateException();
+	}
 }
