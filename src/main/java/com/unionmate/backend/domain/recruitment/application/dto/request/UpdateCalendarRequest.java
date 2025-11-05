d6ad715260fb95d694fb8345af29b6ff1f908ca0
1winhyun <jimking1@naver.com>
2025-11-05 15:43:03 +0900

    refactor: recruitement 업데이트 관련 request dto 스키마 어노테이션 추가

--- src/main/java/com/unionmate/backend/domain/recruitment/application/dto/request/UpdateCalendarRequest.java
+++ src/main/java/com/unionmate/backend/domain/recruitment/application/dto/request/UpdateCalendarRequest.java
@@ -2,15 +2,27 @@ package com.unionmate.backend.domain.recruitment.application.dto.request;
 
 import java.time.LocalDate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateCalendarRequest(
 	@NotNull
+	@Schema(description = "항목 id", example = "1")
 	Long id,
+
+	@Schema(description = "필수 여부", example = "true")
 	Boolean required,
+
+	@Schema(description = "항목 이름", example = "가능한 날짜")
 	String title,
+
+	@Schema(description = "항목 순서", example = "1")
 	Integer order,
+
+	@Schema(description = "항목 설명", example = "희망하는 날짜를 선택해주세요")
 	String description,
+
+	@Schema(description = "날짜", example = "2025-11-30")
 	LocalDate date
 ) implements UpdateItemRequest{
 }
--- src/main/java/com/unionmate/backend/domain/recruitment/application/dto/request/UpdateSelectOptionRequest.java
+++ src/main/java/com/unionmate/backend/domain/recruitment/application/dto/request/UpdateSelectOptionRequest.java
@@ -1,10 +1,21 @@
 package com.unionmate.backend.domain.recruitment.application.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UpdateSelectOptionRequest(
+	@Schema(description = "선택지 id", example = "1")
 	Long id,
+
+	@Schema(description = "선택지 이름", example = "월요일")
 	String title,
+
+	@Schema(description = "선택지 순서", example = "1")
 	Integer order,
+
+	@Schema(description = "기타 여부", example = "true")
 	Boolean isEtc,
+
+	@Schema(description = "기타 항목 제목", example = "기타(입력해주세요.)")
 	String etcTitle
 ) {
 }
