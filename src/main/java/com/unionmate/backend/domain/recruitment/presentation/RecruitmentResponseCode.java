cb447179ef17d9dec9c01f640b5cefd3257efca2
1winhyun <jimking1@naver.com>
2025-11-06 22:41:27 +0900

    feat: 지원서 양식들 조회 기능 controller 구현

--- src/main/java/com/unionmate/backend/domain/recruitment/presentation/RecruitmentResponseCode.java
+++ src/main/java/com/unionmate/backend/domain/recruitment/presentation/RecruitmentResponseCode.java
@@ -12,6 +12,7 @@ import lombok.Getter;
 public enum RecruitmentResponseCode implements ResponseCodeInterface {
 	CREATE_RECRUITMENT(200, HttpStatus.OK, "지원서 양식 생성에 성공했습니다."),
 	UPDATE_RECRUITMENT(200, HttpStatus.OK, "지원서 양식 수정에 성공했습니다."),
+	GET_RECRUITMENTS(200, HttpStatus.OK, "지원서 양식 목록 조회에 성공했습니다."),
 	GET_RECRUITMENT(200, HttpStatus.OK, "지원서 양식 조회에 성공했습니다."),
 	DELETE_RECRUITMENT(200, HttpStatus.OK, "지원서 삭제에 성공했습니다."),
 	RECRUITMENT_TOGGLE_ACTIVATION(200, HttpStatus.OK, "학생회 모집 게시 상태 변경에 성공했습니다.");
