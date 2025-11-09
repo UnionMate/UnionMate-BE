25effb141cd3aaa49ee5fd1e1eb672b8db34a20d
1winhyun <jimking1@naver.com>
2025-11-09 23:05:13 +0900

    refactor: 필요없는 임포트문 삭제

--- src/main/java/com/unionmate/backend/domain/recruitment/application/dto/request/UpdateCalendarRequest.java
+++ src/main/java/com/unionmate/backend/domain/recruitment/application/dto/request/UpdateCalendarRequest.java
@@ -1,6 +1,5 @@
 package com.unionmate.backend.domain.recruitment.application.dto.request;
 
-import java.time.LocalDate;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
